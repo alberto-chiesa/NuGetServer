1c606433d5f7bbdd58db0d57ce601cc8cdd0545c
erikkallen <erik_kallen@hotmail.com>
2011-12-07 01:19:49 +0100

    More work on the users functionality

--- NuGetServer/Scripts/bootstrap-dropdown.js
+++ NuGetServer/Scripts/bootstrap-dropdown.js
@@ -18,38 +18,39 @@
  * ============================================================ */
 
 
-!function( $ ){
+!function ($) {
 
-  "use strict"
+	"use strict"
 
-  /* DROPDOWN PLUGIN DEFINITION
-   * ========================== */
+	/* DROPDOWN PLUGIN DEFINITION
+	* ========================== */
 
-  $.fn.dropdown = function ( selector ) {
-    return this.each(function () {
-      $(this).delegate(selector || d, 'click', function (e) {
-        var li = $(this).parent('li')
+	$.fn.dropdown = function (selector) {
+		return this.each(function () {
+			$(this).delegate(selector || d, 'click', function (e) {
+				debugger;
+				var li = $(this).parent('li')
           , isActive = li.hasClass('open')
 
-        clearMenus()
-        !isActive && li.toggleClass('open')
-        return false
-      })
-    })
-  }
+				clearMenus()
+				!isActive && li.toggleClass('open')
+				return false
+			})
+		})
+	}
 
-  /* APPLY TO STANDARD DROPDOWN ELEMENTS
-   * =================================== */
+	/* APPLY TO STANDARD DROPDOWN ELEMENTS
+	* =================================== */
 
-  var d = 'a.menu, .dropdown-toggle'
+	var d = 'a.menu, .dropdown-toggle'
 
-  function clearMenus() {
-    $(d).parent('li').removeClass('open')
-  }
+	function clearMenus() {
+		$(d).parent('li').removeClass('open')
+	}
 
-  $(function () {
-    $('html').bind("click", clearMenus)
-    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' )
-  })
+	$(function () {
+		$('html').bind("click", clearMenus)
+		$('body').dropdown('[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle')
+	})
 
-}( window.jQuery || window.ender );
+} (window.jQuery || window.ender);
